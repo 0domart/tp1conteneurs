eb57b477b5920124ab744b995315ea839b41b033
Julien GODEST <49200403+GodestJulien@users.noreply.github.com>
2019-11-21 16:39:38 +0100

    Update GestionExpeditions.java

--- src/main/java/fr/univamu/iut/exo1/GestionExpeditions.java
+++ src/main/java/fr/univamu/iut/exo1/GestionExpeditions.java
@@ -1,7 +1,12 @@
 package fr.univamu.iut.exo1;
 
+import fr.univamu.iut.Conteneur;
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class GestionExpeditions {
     public static void main(String[] args) {
-        throw new RuntimeException("Not yet implemented !");
+
+        Collection<Conteneur> conteneurs = Lecteur.charger("Conteneurs.ser");
     }
 }
